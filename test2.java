63ce99104adf399ebd26d31b0c39d3b077baf7c6
Philip Hoffmann <phihoffix@gmail.com>
2026-04-27 11:59:15 +0200

    test

---
+++ test2.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse test2.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class test2
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse test2
+     */
+    public test2()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
